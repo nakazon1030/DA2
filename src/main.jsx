3bf254bba5da8124594c8ce4f0923c8f111f59fc
nakazon1030 <nakazon1111@gmail.com>
2024-11-21 15:00:33 +0900

    ちょっとできてきた

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
-import RoutePage from './RoutePage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Routes>
+    <BrowserRouter>
+      <Routes>
         <Route path="/" element={<App />} />
-    </Routes>
-    
+      </Routes>
+    </BrowserRouter>
   </StrictMode>,
 )
