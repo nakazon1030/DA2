f5b4495f88b99914371165de7a3c8c49b8e344ba
nakazon1030 <nakazon1111@gmail.com>
2024-11-21 15:23:37 +0900

    finish

--- src/Details.jsx
+++ src/Details.jsx
@@ -2,29 +2,22 @@ import { useState } from 'react'
 import { Routes, Route } from 'react-router-dom';
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import { useNavigate } from 'react-router-dom';
 import './App.css'
 
-function App() {
-  const navigate = useNavigate();
-
-  const handleClick = () => {
-    // ボタンがクリックされたときに遷移
-    navigate('/x');
-  };
+function Details() {
 
   return (
     <>
       <div className="top-area">
-        <h1>aaaaa</h1>
+        <h1>Details</h1>
         <div className="book-area">
           <p className="title">title</p>
           <p>\1000</p>
-          <button className="button" onClick={handleClick}>詳細</button>
+          <button className="button">詳細</button>
         </div>
       </div>
     </>
   )
 }
 
-export default App
+export default Details
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
+import Details from './Details.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/x" element={<Details />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
