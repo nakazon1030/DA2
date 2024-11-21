1591c3ee8440d1120dcab95fc9020ed733460b5c
nakazon1030 <nakazon1111@gmail.com>
2024-11-21 17:31:03 +0900

    not.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './not.css'
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
 import BookDetail from './BookDetail.jsx';
