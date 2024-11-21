2cfaba82c216d131ac7891f38243be973366bf7c
nakazon1030 <nakazon1111@gmail.com>
2024-11-21 14:57:48 +0900

    route

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { Routes, Route } from 'react-router-dom';
 import App from './App.jsx'
+import RoutePage from './RoutePage.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Routes>
+        <Route path="/" element={<App />} />
+    </Routes>
+    
   </StrictMode>,
 )
