b0d95387f38cb3b3040c7f57c7281d0f350ea2ab
nakazon1030 <nakazon1111@gmail.com>
2024-11-27 14:46:03 +0900

    帯変更システム作ってみた

--- src/main.jsx
+++ src/main.jsx
@@ -2,26 +2,20 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './not.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App.jsx'
-
-import BookDetail from './BookDetail.jsx';
-import MyPage from './MyPage.jsx';
-import Message from './Message.jsx';
-
-import Myreport from './Myreport.jsx';
-
+import Myreport from './Myreport.jsx'
+import TestPage from './TestPage.jsx'
+import Edit from './Edit.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/BookDetail" element={<BookDetail />} />
-        <Route path="/MyPage" element={<MyPage />} />
-        <Route path="/Message" element={<Message />} />
         <Route path='/Myreport' element={<Myreport />} />
-
+        <Route path='/TestPage' element={<TestPage />} />
+        <Route path='/Edit' element={<Edit />} />
       </Routes>
     </BrowserRouter>
 
